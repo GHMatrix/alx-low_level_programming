2d4df7acf9b9f2cf751247b40c7bb8ee922cf894
GHMatrix <gsmensah@yahoo.com>
2023-02-16 06:41:02 -0800

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  Program to print message using puts function followed by a new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
