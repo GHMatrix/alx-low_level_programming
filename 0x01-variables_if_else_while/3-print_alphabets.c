a30b1447e62ad51a4695fbf6637e6a6ae51aca84
GHMatrix <gsmensah@yahoo.com>
2023-02-17 08:14:38 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Program prints alphabets in lower case then upper cases then a line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char d;
+
+	char i;
+
+	d = 'a';
+	i = 'A';
+	while
+		(d < 'z') {
+			putchar(d);
+			d++;
+		}
+	while
+		(i <= 'Z') {
+			putchar(i);
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
