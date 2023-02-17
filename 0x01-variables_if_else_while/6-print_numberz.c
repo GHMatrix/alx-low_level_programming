d9b0915e5980d0f191097806822498733f7cb10a
GHMatrix <gsmensah@yahoo.com>
2023-02-17 09:50:49 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Program prints all single digit numbers of base10
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	while
+		(i < 10) {
+			putchar(i + '0');
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
