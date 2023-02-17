fa3c01c915cbeb0f6bad76327507a93ec536d07f
GHMatrix <gsmensah@yahoo.com>
2023-02-17 10:06:25 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Program prints lowercase alphabets in reverse
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	i = 'z';
+	while
+		(i >= 'a') {
+			putchar(i);
+			i--;
+		}
+	putchar('\n');
+	return (0);
+}
