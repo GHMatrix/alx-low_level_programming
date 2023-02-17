b5c0e190db552af48e6c85d22663cd8a1b3684e8
GHMatrix <gsmensah@yahoo.com>
2023-02-17 07:28:43 -0800

    Insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Program that prints alphabets in lower case with new line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char d;
+
+	d = 'a';
+	while
+		(d <= 'z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
+
