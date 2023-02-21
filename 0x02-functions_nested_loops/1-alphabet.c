33ab4d24ee2f55434d39e8fea62931248dab5bc9
GHMatrix <gsmensah@yahoo.com>
2023-02-21 06:58:48 -0800

    Suffer from Insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - Program prints alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	for (a <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+		_putchar('\n');
+	}
+	return (void);
