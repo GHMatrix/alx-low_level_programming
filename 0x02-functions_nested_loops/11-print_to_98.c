0e5eab059217e5c762fac088649707094a74dd3f
GHMatrix <gsmensah@yahoo.com>
2023-02-21 15:42:10 -0800

    Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - Prints natural numbers
  * @n: The starting number
@@ -27,11 +28,11 @@ void print_to_98(int n)
 			for (; n >= 98; n--)
 			{
 				if (n == 98)
-				{
+			{
 				printf("%d", n);
 				printf("\n");
 				break;
-				}
+			}
 			else
 			{
 				printf("%d, ", n);
