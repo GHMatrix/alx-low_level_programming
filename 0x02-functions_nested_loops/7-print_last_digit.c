2646a3d230ef8cb4211cdde3c59c2b5b477773ff
GHMatrix <gsmensah@yahoo.com>
2023-02-21 11:49:17 -0800

    There are only 3 colors

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints last digit of a number
+ * @n: The number to be checked
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+	_putchart(last_digit + '0');
+	return (last_digit);
+}
