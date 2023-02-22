10110dac5ca45a02e7782fffdca70a23cdc3cb1c
GHMatrix <gsmensah@yahoo.com>
2023-02-22 08:34:41 -0800

    Even Liber Abacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,18 +6,16 @@
 int main(void)
 {
 	int i = 0;
-	long j = 1;
-	long k = 2;
-	long sum = k;
+	long j = 1, k = 2, sum = k;
 
 	while (k + j < 4000000)
 	{
 		k += j;
 		if (k % 2 == 0)
-			sum += k;
+		sum += k;
 		j = k - j;
 		++i;
 	}
-	printf("%ld\n"' sum);
+	printf("%ld\n", sum);
 	return (0);
 }
