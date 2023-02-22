bf39354389df0a63e44be2d305d9aa9c355bae4d
GHMatrix <gsmensah@yahoo.com>
2023-02-21 17:02:30 -0800

    Nature made

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints sum of all multiples of 3 or 5 to 1024
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			j += i;
+		}
+		i++;
+	}
+	printf("%d\n", j);
+	return (0);
+}
