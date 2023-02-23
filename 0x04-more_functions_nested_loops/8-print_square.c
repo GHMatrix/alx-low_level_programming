61fd481357710cd00a211af884cb5ef0377869fa
GHMatrix <gsmensah@yahoo.com>
2023-02-23 08:34:02 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - Program prints a square, followed by a new line
+ * @size: Number of squares for size
+ * Return: 0 (Success)
+ */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+	{
+		_putchar9('\n');
+	}
+		else
+		{
+			for (a = 0; a < size; a++)
+			{
+				for (b = 0; b < size; b++)
+				{
+					_putchar(35);
+				}
+				_putchar('\n');
+			}
+		}
+}
