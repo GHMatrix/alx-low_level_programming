590ddc8ac2c68f5f36a858183ab3061d2c5d5e67
GHMatrix <gsmensah@yahoo.com>
2023-02-23 04:51:50 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints numbers from 0 up to 9
+ * Return: The numbers from 0 to 9
+ */
+void print_most_numbers(void)
+{
+	int c = 0;
+
+	for (; c <= 9; c++)
+	{
+		if (c == 2 || c == 4)
+		{
+			continue;
+		}
+		else
+		{
+			putchar(c + '0');
+		}
+	}
+	_putchar('\n');
+}
