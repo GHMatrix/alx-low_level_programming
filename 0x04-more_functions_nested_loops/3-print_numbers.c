fa2c13d5cb64550ed533e02f220d93c8fa2cc252
GHMatrix <gsmensah@yahoo.com>
2023-02-23 03:11:30 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Prints numbers from 0 to 9 followed by new line
+ * Return: The numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
