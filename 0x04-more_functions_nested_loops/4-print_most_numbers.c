53995efe8138cbfb915657fbe21df71197b99002
GHMatrix <gsmensah@yahoo.com>
2023-02-23 04:56:25 -0800

    I believe in numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * print_most_numbers - Prints numbers from 0 up to 9
+ * Description: Prints numbers without 2 and 4
  * Return: The numbers from 0 to 9
  */
 void print_most_numbers(void)
