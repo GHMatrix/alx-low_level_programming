df700b83092b801885cd6af809c032c0d56da7ae
GHMatrix <gsmensah@yahoo.com>
2023-03-02 02:59:01 -0800

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - Program concatenates two strings
+ * @src: first string value
+ * @dest: second string value
+ * Return: 0 (Success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (src[b] != '\0')
+	{
+	dest[a] = src[b];
+	a++;
+	b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+This is a readme file for 0x06. C - More pointers, arrays and strings
+
