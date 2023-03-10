2f2ed64e39d67a87587939c3497b87f5df6712d3
GHMatrix <gsmensah@yahoo.com>
2023-03-10 11:31:45 -0800

    Slience is argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Program prints the number of arguments passed into it.
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: 0
+ */
+int main(int argc, *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
