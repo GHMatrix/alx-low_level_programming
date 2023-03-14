ed1de105369cdac7c087e62ba5e354467d06c026
GHMatrix <gsmensah@yahoo.com>
2023-03-14 04:56:59 -0700

    If you even dream of beating me

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ben = malloc(sizeof(int) * height);
+	ben = malloc(sizeof(int *) * height);
 
 	if (ben == NULL)
 		return (NULL);
