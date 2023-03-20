dc6d87deaa672393821827897d77a5633b6246b4
GHMatrix <gsmensah@yahoo.com>
2023-03-20 13:24:29 -0700

    Defining a new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,14 +16,14 @@ struct dog
 };
 
 /**
- * dog_ty - typedef for struct doginfo
+ * dog_t - typedef for struct doginfo
  */
-typedef struct dog dog_ty;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_ty *new_dog(char *name, float age, char *owner);
-void free_dog(dog_ty *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
