edb4d9f332c04239a5d264b86e70e96ed312902b
GHMatrix <gsmensah@yahoo.com>
2023-07-26 15:53:25 -0700

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,49 +1,28 @@
-def island_perimeter(grid):
-    """
-    Calculate the perimeter of an island represented by a grid.
+#!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
 
-    The grid represents water by 0 and land by 1. The perimeter of the island
-    is the total length of the boundaries
-    that separate the land from the water.
+
+def island_perimeter(grid):
+    """Return the perimiter of an island.
+
+    The grid represents water by 0 and land by 1.
 
     Args:
-        grid (list[list[int]]): A 2D grid representing the island.
-
+        grid (list): A list of list of integers representing an island.
     Returns:
-        int: The perimeter of the island.
-
-    Example:
-        Example_grid = [
-            [0, 1, 0, 0],
-            [1, 1, 1, 0],
-            [0, 1, 0, 0],
-            [1, 1, 0, 0]
-        ]
-        island_perimeter(example_grid)  # Output: 16
+        The perimeter of the island defined in grid.
     """
-    def count_neighbors(row, col):
-        """
-        Count the number of neighboring land cells around a given cell.
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-        Args:
-            row (int): The row index of the cell.
-            col (int): The column index of the cell.
-
-        Returns:
-            int: The number of neighboring land cells.
-        """
-        count = 0
-        for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            nr, nc = row + dr, col + dc
-            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
-                count += grid[nr][nc]
-        return count
-
-    perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                neighbors = count_neighbors(i, j)
-                perimeter += 4 - neighbors
-
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
