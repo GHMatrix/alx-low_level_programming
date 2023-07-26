84af13536be266fa98f60c261d5948e53ad04ab7
GHMatrix <gsmensah@yahoo.com>
2023-07-26 15:46:32 -0700

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,45 +1,49 @@
-#!/usr/bin/python3
-
 def island_perimeter(grid):
     """
-    Calculates the perimeter of the island described in the grid.
+    Calculate the perimeter of an island represented by a grid.
+
+    The grid represents water by 0 and land by 1. The perimeter of the island
+    is the total length of the boundaries
+    that separate the land from the water.
 
     Args:
-        grid (List[List[int]]): A list of lists of
-        integers representing the island.
+        grid (list[list[int]]): A 2D grid representing the island.
 
     Returns:
         int: The perimeter of the island.
 
-    Note:
-        0 represents a water zone.
-        1 represents a land zone.
-        One cell is a square with side length 1.
-        Grid cells are connected horizontally/vertically (not diagonally).
-        Grid is rectangular, width and height don’t exceed 100.
-        Grid is completely surrounded by water,
-        and there is one island (or nothing).
-        The island doesn’t have “lakes” (water inside
-        that isn’t connected to the water around the island).
+    Example:
+        Example_grid = [
+            [0, 1, 0, 0],
+            [1, 1, 1, 0],
+            [0, 1, 0, 0],
+            [1, 1, 0, 0]
+        ]
+        island_perimeter(example_grid)  # Output: 16
     """
+    def count_neighbors(row, col):
+        """
+        Count the number of neighboring land cells around a given cell.
+
+        Args:
+            row (int): The row index of the cell.
+            col (int): The column index of the cell.
+
+        Returns:
+            int: The number of neighboring land cells.
+        """
+        count = 0
+        for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            nr, nc = row + dr, col + dc
+            if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                count += grid[nr][nc]
+        return count
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+                neighbors = count_neighbors(i, j)
+                perimeter += 4 - neighbors
+
     return perimeter
-
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))
